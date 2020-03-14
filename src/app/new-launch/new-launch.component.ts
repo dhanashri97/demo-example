4e9022deb61610987c18b33dcfb61519ef735d79
dhanashri kokate <dhanashrikokate25@gmail.com>
2020-03-14 17:37:39 +0530

    angular/material demo example(toolbar,input,datepicker)

---
+++ src/app/new-launch/new-launch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-launch',
+  templateUrl: './new-launch.component.html',
+  styleUrls: ['./new-launch.component.css']
+})
+export class NewLaunchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
